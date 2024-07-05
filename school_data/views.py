9411e9df226478e953badcf55edf2d56820c7c6a
prince <sainiprince6760@gmail.com>
2024-07-05 21:44:21 +0530

    my first change

--- school_data/views.py
+++ school_data/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .forms import StudentRegistration
 
-# Create your views here.
+# Create your views here
 def addandshort(request):
     fm=StudentRegistration()
     return render(request,'test_app/addandshort.html',{'form':fm})
